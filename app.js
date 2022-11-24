c75647c3a52dd0095f0469fb156a79c143afb6e0
gbstorey <gbstorey@asu.edu>
2022-11-23 18:24:06 -0700

    All three view engine files

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const { engine } = require("express-handlebars");
+// const { engine } = require("express-handlebars");
 
 const app = express();
 
-app.engine("handlebars", engine({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", engine({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
+// app.set("view engine", "handlebars");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -20,7 +21,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "",});
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,8 @@
-const path = require("path");
+// const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
 
 const router = express.Router();
 
